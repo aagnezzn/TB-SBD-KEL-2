54b68fa630168ed2ba07bff3a987e87941e0035b
Agnes Natalia Siregar <agnes10perkantoran@gmail.com>
2026-04-28 21:52:49 +0700

    Update

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="bg-white border-b border-gray-200 px-6 py-2 flex items-center h-16 w-full relative">
 
-    <div class="flex items-center space-x-4 shrink-0">
+    <div class="flex items-center space-x-4 flex-shrink-0">
         <h1 class="text-2xl font-bold text-black cursor-pointer">idemy</h1>
 
         <div class="relative flex items-center h-full" 
@@ -14,7 +14,7 @@
     
     <div x-show="showMenu" 
          x-cloak
-         class="absolute left-0 top-full w-[850px] bg-white border border-gray-200 shadow-2xl z-999 flex rounded-sm"
+         class="absolute left-0 top-full w-[850px] bg-white border border-gray-200 shadow-2xl z-[999] flex rounded-sm"
          style="min-height: 450px;">
         
         <div class="w-1/3 border-r border-gray-100 py-4 bg-white">
@@ -45,7 +45,7 @@
         <a href="#" class="text-sm hover:text-purple-600 whitespace-nowrap">Berlangganan</a>
     </div>
 
-    <div class="grow mx-6">
+    <div class="flex-grow mx-6">
         <div class="relative w-full">
             <span class="absolute inset-y-0 left-4 flex items-center">
                 <i data-feather="search" class="w-4 h-4 text-gray-400"></i>
@@ -56,7 +56,7 @@
         </div>
     </div>
 
-    <div class="flex items-center space-x-3 shrink-0">
+    <div class="flex items-center space-x-3 flex-shrink-0">
     <div class="relative" x-data="{ open: false }">
     <a href="#" 
        @mouseenter="open = true" 
@@ -70,7 +70,7 @@
          @mouseleave="open = false"
          x-transition
          x-cloak
-         class="absolute left-0 top-full w-56 bg-white border border-gray-200 shadow-lg py-2 z-1000 rounded-sm flex flex-col gap-1">
+         class="absolute left-0 top-full w-56 bg-white border border-gray-200 shadow-lg py-2 z-[1000] rounded-sm flex flex-col gap-1">
         
         <a href="#" class="px-4 py-2 text-sm text-gray-700 hover:text-purple-700">
             Bandingkan Paket
@@ -93,7 +93,7 @@
     <div x-show="open" 
          x-transition
          x-cloak
-         class="absolute right-0 top-full w-72 bg-white border border-gray-200 shadow-lg p-4 z-1000 rounded-sm text-center flex flex-col gap-3">
+         class="absolute right-0 top-full w-72 bg-white border border-gray-200 shadow-lg p-4 z-[1000] rounded-sm text-center flex flex-col gap-3">
         
         <p class="text-gray-800 font-bold text-base leading-snug">
             Jadikan pengetahuan Anda sebagai peluang untuk menjangkau jutaan orang di seluruh dunia.
@@ -127,7 +127,7 @@
     <div x-show="openModal"
      x-transition.opacity
      x-cloak
-     class="fixed inset-0 z-9999 bg-black/20 backdrop-blur-sm flex items-center justify-center"
+     class="fixed inset-0 z-[9999] bg-black/20 backdrop-blur-sm flex items-center justify-center"
      @click.self="openModal = false">
         
         <div x-show="openModal" 
