4e24c43241346cbc951d0d27c05f75326b67cebc
agnes <agneszsiregar@gmail.com>
2026-06-09 21:11:38 +0700

    ay ay kapten

--- database/factories/CourseFactory.php
+++ database/factories/CourseFactory.php
@@ -37,10 +37,20 @@ class CourseFactory extends Factory
             $instructorId = User::factory()->create(['role' => 'instructor'])->id;
         }
 
+        // FAKTA PERBAIKAN: Array gambar statis bertema coding agar tidak kembar dan nyambung dengan Idemy
+        $gambarCoding = [
+            'https://images.unsplash.com/photo-1498050108023-c5249f4df085?w=640&q=80',
+            'https://images.unsplash.com/photo-1555066931-4365d14bab8c?w=640&q=80',
+            'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?w=640&q=80',
+            'https://images.unsplash.com/photo-1526045612212-70caf35c14df?w=640&q=80',
+            'https://images.unsplash.com/photo-1461749280684-dccba630e2f6?w=640&q=80',
+            'https://images.unsplash.com/photo-1504639725590-34d0984388bd?w=640&q=80',
+        ];
+
         return [
             'title' => $title,
             'description' => "This is a specialized course about " . $catName . ". Designed for all levels.",
-            'image_url' => 'https://loremflickr.com/640/360/computer,office/all?lock=' . rand(1, 1000),
+            'image_url' => fake()->randomElement($gambarCoding), // Sistem akan mengambil acak 1 link dari array $gambarCoding di atas
             'category_id' => $category->id,
             'instructor_id' => $instructorId,
             'price' => $this->faker->randomElement([150000, 250000, 350000, 500000]),
